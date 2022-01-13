3b96fbe9c2542bf33b758717fe1813195a562930
Michael von der Lippe <mvonderlippe@ansfederal.com>
2022-01-13 07:37:28 -0500

    Added UserById Implementation and Test

--- client/users.go
+++ client/users.go
@@ -39,6 +39,31 @@ func (c *UserClient) GetUsers() (*Users, error) {
 	return &users, nil
 }
 
-func (c *UserClient) GetUser(userId string) (*User, error) {
-	return nil, nil
+func (c *UserClient) GetUserById(userId string) (*User, error) {
+  userByIdUrl := fmt.Sprintf("%s%s/users/%s", c.BaseUrl, c.APIVersion, userId)
+  
+  req, err := http.NewRequest("GET", userByIdUrl, nil)
+  if err != nil {
+    return nil, err
+  }
+
+  authHeader := fmt.Sprintf("Bearer %s", c.authToken)
+  req.Header.Add("Authorization", authHeader)
+
+  res, err := c.httpClient.Do(req)
+  if err != nil {
+    return nil, err
+  }
+  defer res.Body.Close()
+  if res.StatusCode != 200 {
+    return nil, fmt.Errorf("get user by id failed. status code: %d", res.StatusCode)
+  }
+
+  var user User
+  err = json.NewDecoder(res.Body).Decode(&user)
+  if err != nil {
+    return nil, err
+  }
+
+  return &user, nil
 }
--- client/users_test.go
+++ client/users_test.go
@@ -19,3 +19,19 @@ func Test_GetUsers(t *testing.T) {
   
   t.Log(users)
 }
+
+func Test_GetUserById(t *testing.T) {
+  // Setup
+  config, _ := ConfigurationFromFile("../config.json")
+  c := NewClient(config.SubDomain, config.APIVersion)
+  c.Authenticate(config.Username, config.Password, config.APIKey, config.APISecret)
+  uc := &UserClient{Client: *c}
+
+  // Test
+  user, err := uc.GetUserById("1") // Any/All Tenants should have a user with id 1
+  if err != nil {
+    t.Errorf("GetUserById failed: %s", err)
+  }
+
+  t.Log(user)
+}
